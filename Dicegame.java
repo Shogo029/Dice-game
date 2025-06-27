f898a09f95e65c6fd167d9c57a5a136f4771d1d7
YOSHIDA Shogo <s1320175@sshsv212.u-aizu.ac.jp>
2025-06-27 12:50:38 +0900

    Won/lost message added

--- Dicegame.java
+++ Dicegame.java
@@ -1,14 +1,16 @@
 class Dicegame{
     public static void main(String[] args){
+
         double rand = Math.random() * 6 + 1;
         int die1 = (int)rand;
-
         rand = Math.random() * 6 + 1;
         int die2 = (int)rand;
-
         System.out.println("Rolling dice…");
         System.out.println("Die 1: "+ die1);
         System.out.println("Die 2: "+ die2);
         System.out.println("Total value: "+ (die1+die2));
+
+        if(die1+die2 > 7) System.out.println("You won");
+        else System.out.println("You lost");
     }
 } 
